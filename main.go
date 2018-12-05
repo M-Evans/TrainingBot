9ce2d3930c4548e6ba3f0d96876312d5a735a5f3
root <root@evans.systems>
2018-12-05 04:14:36 +0000

    rename

--- main.go
+++ main.go
@@ -1,31 +1,74 @@
 package main
 
 import (
-	//"encoding/json"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	//"log"
+	"io/ioutil"
 	"net/http"
-	"strings"
+
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
-	w.Write([]byte(message))
-	/*
-		json, err := json.MarshalIndent(r, "", "    ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Fprintf(w, "\n%s\n", string(json))
-		// */
-	fmt.Fprintf(w, "PostForm: %+v\n", (*r).PostForm)
+// You more than likely want your "Bot User OAuth Access Token"
+func getOauthToken() string {
+	dat, err := ioutil.ReadFile("./secrets/oauth")
+	if (err != nil) { panic(err) }
+	var ret = string(dat)
+	return ret[:len(ret)-1]
+}
+
+func getVerificationToken() string {
+	dat, err := ioutil.ReadFile("./secrets/slack")
+	if (err != nil) { panic(err) }
+	var ret = string(dat)
+	return ret[:len(ret)-1]
 }
 
 func main() {
-	http.HandleFunc("/slack/event", sayHello)
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	var oauthToken = getOauthToken();
+	var verificationToken = getVerificationToken();
+	fmt.Printf("oauthToken: '%s'", oauthToken)
+	fmt.Printf("verificationToken: '%s'", verificationToken)
+	var api = slack.New(oauthToken);
+
+	http.HandleFunc("/slack/event", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("handling event")
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		body := buf.String()
+		fmt.Println("body:")
+		fmt.Println(body)
+
+		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body),
+					slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: verificationToken}))
+		if e != nil {
+			fmt.Println(e)
+			fmt.Println("internal server error - parseEvent")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				fmt.Println("internal server error - unmarshal request json")
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+				case *slackevents.AppMentionEvent:
+					api.PostMessage(ev.Channel, slack.MsgOptionText("At your service.", false))
+			}
+		}
+	})
+	fmt.Println("[INFO] Server listening")
+	if err := http.ListenAndServe(":710", nil); err != nil {
 		panic(err)
 	}
 }
