d6b8d0e006ad1b844dd77984fa0999587e68c59d
root <root@evans.systems>
2018-12-06 05:19:09 +0000

    weight regex

--- event.go
+++ event.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"io/ioutil"
 
 	"github.com/nlopes/slack"
@@ -16,6 +17,27 @@ func getOauthToken() string {
 	return ret[:len(ret)-1]
 }
 
+func init() {
+	var oauthToken = getOauthToken();
+	var api = slack.New(oauthToken);
+	api.PostMessage("CCUDKKFT5", slack.MsgOptionText("_*info* - I have restarted_", false))
+}
+
+func checkWeightMeasurement(message string) {
+	var oauthToken = getOauthToken();
+	var api = slack.New(oauthToken);
+	var re, err = regexp.MatchString("^\\d+\\.\\d+ this morning$", message)
+	if (err != nil) {
+		api.PostMessage("CCUDKKFT5", slack.MsgOptionText("This is too much to handle. I quit.", false))
+		panic(err)
+	}
+	if (re) {
+		api.PostMessage("CCUDKKFT5", slack.MsgOptionText("I received a weight measurement", false))
+	} else {
+		api.PostMessage("CCUDKKFT5", slack.MsgOptionText("I did not receive a weight measurement", false))
+	}
+}
+
 func handle(event slackevents.EventsAPIInnerEvent) {
 	var oauthToken = getOauthToken();
 	var api = slack.New(oauthToken);
@@ -26,7 +48,8 @@ func handle(event slackevents.EventsAPIInnerEvent) {
 	case *slackevents.MessageEvent:
 		fmt.Printf("ev: %v", ev)
 		if (ev.Channel == "CCUDKKFT5" && ev.BotID == "") {
-			api.PostMessage(ev.Channel, slack.MsgOptionText(ev.Text, false))
+			checkWeightMeasurement(ev.Text)
+			// api.PostMessage(ev.Channel, slack.MsgOptionText(ev.Text, false))
 		}
 	}
 }
